a502d15a1cedd667c8973af63f6eea442244246f
Lana Holmes <lana.lee.holmes@gmail.com>
2019-02-01 01:33:27 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -50,7 +50,7 @@ class MusicLibraryController
   end
   
   def list_songs
-    song_array = Song.all.sort_by! {collect { |song| song.name }.sort
+    song_array = Song.all.sort_by! { |song| song.name } #collect { |song| song.name }.sort
     puts song_array
     song_array.each_with_index do |song, i|
      # "1. Thundercat - ForLove I Come - dance"
